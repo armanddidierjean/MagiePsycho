7153143eaec411ce87918782956a4ec823cb15b8
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-01-20 20:44:56 +0100

    Index videos from 1 and list existing videos

--- pages/videos/[id].vue
+++ pages/videos/[id].vue
@@ -9,8 +9,14 @@
     <div class="p-4">{{ video.description }}</div>
 
     <div class="flex flex-row justify-items-stretch w-max">
-      <UButton :to="String(videoId - 1)" :disabled="videoId == 0">Vidéo Précédente</UButton>
-      <UButton :to="String(videoId + 1)" :disabled="videoId >= videoList.length - 1">Vidéo suivante</UButton>
+      <UButton :to="String(videoId - 1)" :disabled="videoId <= 1">Vidéo Précédente</UButton>
+      <UButton :to="String(videoId + 1)" :disabled="videoId >= videoList.length">Vidéo suivante</UButton>
+    </div>
+
+    <div class="flex flex-col m-10">
+      <NuxtLink class="hover:underline" v-for="video, index in videoList" to="/">
+        {{ index }} - {{ video.title }}
+      </NuxtLink>
     </div>
   </div>
 </template>
@@ -21,13 +27,13 @@ const route = useRoute()
 const videoList = [
   {
     title: "Première vidéo",
-    description: "Description de la vidéo 0",
+    description: "Description de la vidéo 1",
     url: "https://www.youtube-nocookie.com/embed/vJG698U2Mvo?si=TjIjxbpFQv4SdqzS",
   },
   {
     title: "Seconde vidéo",
-    description: "Description de la vidéo 1",
-    url: "https://www.youtube.com/embed/wkTsl0qZp7g?si=Vbk9LGbKV5eS018q",
+    description: "Description de la vidéo 2",
+    url: "https://www.youtube-nocookie.com/embed/wkTsl0qZp7g?si=Vbk9LGbKV5eS018q",
   }
 ]
 
@@ -36,13 +42,13 @@ console.log(route.params.id)
 
 const videoId = Number(route.params.id)
 
-if (videoId < 0 || videoId >= videoList.length || isNaN(videoId)) {
+if (videoId < 1 || videoId > videoList.length || isNaN(videoId)) {
   // Redirect to the first video if the video_id is invalid
   console.log("Redirecting to the first video")
-  await navigateTo(`/videos/0`)
+  await navigateTo(`/videos/1`)
 }
 
-const video = videoList[videoId] ?? {
+const video = videoList[videoId - 1] ?? {
   title: "",
   description: "",
   url: ""
