80e05706c86af12afbfeacf0861dda2b1cf867b8
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-01-20 21:47:50 +0100

    Special

---
+++ pages/special/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1 class="text-xl text-center p-4">{{ route.params.id }}</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+
+// When accessing /posts/1, route.params.id will be 1
+console.log(route.params.id)
+
+
+</script>
