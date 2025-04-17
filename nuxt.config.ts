6a18c874fa70a5a1724ffac5e5a237fbf1327713
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-04-17 10:07:59 +0200

    NuxtImage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/scripts"],
+  modules: ["@nuxt/ui", "@nuxt/scripts", "@nuxt/image"],
   css: ["~/assets/css/main.css"],
   ssr: true,
 });
