ae0476c660670bdb536ec6df388919c0809c7056
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-04-24 18:29:04 +0200

    Bottom margin in layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <NavBar />
-    <div class="mx-10 md:mx-40 mt-10 flex flex-col items-center justify-center">
+    <div class="mx-10 md:mx-40 mt-10 mb-20 flex flex-col items-center justify-center">
       <slot />
     </div>
   </div>
--- layouts/navbaronly.vue
+++ layouts/navbaronly.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="mb-20">
     <NavBar />
     <slot />
   </div>
