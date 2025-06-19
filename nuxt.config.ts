5a9ea5d918597555042cb3cfd17976410b5b2f05
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-06-19 10:36:28 +0200

    Add umamiAnalytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/scripts", "@nuxt/image"],
+  scripts: {
+    registry: {
+      umamiAnalytics: {
+        websiteId: "c9a62922-c594-4dfd-864e-d7b621f8fd87",
+      },
+    },
+  },
   css: ["~/assets/css/main.css"],
   ssr: true,
 });
